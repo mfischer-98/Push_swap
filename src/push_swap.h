2f525a5e35a56276bb79b82e0115d5452c6b1262
Your Name <you@example.com>
2025-08-07 18:27:01 +0100

    created makefile

---
+++ src/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mefische <mefische@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 16:22:04 by mefische          #+#    #+#             */
+/*   Updated: 2025/08/07 16:22:04 by mefische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../lib/libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+
+#endif
